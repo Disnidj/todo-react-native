06fffd9f1d59fd2d1c533e8a3ee9473f54fa4418
Disni Jayawickrama <it20147778@my.sliit.lk>
2023-07-28 14:43:16 +0530

    commit

---
+++ todoList/components/Task.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const Task = (props) => {
+
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#FFF',
+    borderColor: '#1260CC',
+    borderWidth: 2,
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap'
+  },
+  square: {
+    width: 30,
+    height: 30,
+    backgroundColor: '#29c5f6',
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: '80%',
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: '#55BCF6',
+    backgroundColor: '#3A9BDC',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
+
+export default Task;
